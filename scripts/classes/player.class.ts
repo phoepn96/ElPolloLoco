9aae2551067740e2db177d801ba3476ceb150082
phoepn96 <patrick-hoepner@outlook.de>
2025-07-01 23:15:27 +0200

    added spritesheets

--- scripts/classes/player.class.ts
+++ scripts/classes/player.class.ts
@@ -2,6 +2,9 @@ import { Character } from "./character.superclass";
 import { World } from "./world.class";
 
 export class Player extends Character {
+  speed: number = 2;
+  jumpForce: number = 10;
+
   constructor(
     world: World,
     startingPositionX: number,
